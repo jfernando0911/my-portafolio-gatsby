4bc29c437a1820be39f467fe2bc918da0f74c807
jfernando0911 <jfernando0911@gmail.com>
2020-05-17 16:02:40 -0500

    deleted some comments

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import containerStyles from './styles/containerStyles.module.css';
 import Navbar from '../components/Navbar';
 import Footer from './Footer';
@@ -6,33 +6,11 @@ import Footer from './Footer';
 
 const Container = ({ children }) => {
 
-  // const getDarkValue = localStorage.getItem(JSON.parse('darkMode'));
-  // console.log("Getting Dark Value", getDarkValue);
-
-
-  const getInitialMode = localStorage.getItem('darkMode') || false;
-  const [darkMode, setDarkMode]=useState(JSON.parse(getInitialMode));
-
-  
-  // console.log();
-
-
-  useEffect(()=>{
-  localStorage.setItem('darkMode', JSON.stringify(darkMode));
-
-  // console.log(localStorage.getItem(JSON.parse('darkMode')));
-  
-  }, [darkMode]);
-
-  
 
   return (
 
-    // <div className={darkMode? containerStyles.container :containerStyles.containerDark}>
-    <div className={darkMode? containerStyles.containerDark : containerStyles.container}>
-      <Navbar darkMode={darkMode}/>
-      {/* <input type="checkbox"/>z */}
-  <button onClick={()=> setDarkMode(prevMode => !prevMode)}>{darkMode? "DarkMode": "LightMode"}</button>
+    <div className={containerStyles.container}>
+      <Navbar />
       <div className={containerStyles.childrenContainer}>
         {children}
 
