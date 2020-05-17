c2a156084f2a7229ab697b3af41a029170cac537
jfernando0911 <jfernando0911@gmail.com>
2020-05-17 17:45:35 -0500

    updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,20 +5,16 @@ import mouse from '../imgs/mouse.png';
 import { Link } from 'gatsby';
 
 
-const Navbar = (props) => {
-    console.log("Getting darkMode parameter from the parent: ", props.darkMode);
+const Navbar = () => {
     return (
-        <div className={props.darkMode ? navbar.containerDark : navbar.container}>
-            <div className={navbar.logoContainer}><img src={mouse} alt={"emoji of a mouse"} /><h3>Fernan Design</h3></div>
+        <div className={navbar.container}>
+            <div className={navbar.logoContainer}><img src={mouse} alt={"emoji of a mouse"} /><h3>JFernando</h3></div>
             <ul className={navbar.ulContainer}>
                 <div className={navbar.linkContainer}>
-                    <Link to="/" className={props.darkMode? navbar.linkDark : navbar.linkLight}>About me</Link>
+                    <Link to="/projects" className={navbar.linkLight}>Work</Link>
                 </div>
                 <div className={navbar.linkContainer}>
-                    <Link to="/work" className={props.darkMode? navbar.linkDark : navbar.linkLight}>Work</Link>
-                </div>
-                <div className={navbar.linkContainer}>
-                    <Link to="/contact" className={props.darkMode? navbar.linkDark : navbar.linkLight}>Contact</Link>
+                    <Link to="/contact" className={navbar.linkLight}>Contact</Link>
                 </div>
             </ul>
         </div>
