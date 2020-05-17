650f3326dbba246d8dcb3df92b590bc4745e484e
jfernando0911 <jfernando0911@gmail.com>
2020-05-17 17:46:39 -0500

    set new classes for the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,22 +6,28 @@ const contact = () => {
     return (
         <div>
             <Container>
-                <div className={contactPage.container}>
-                    <div>
+
+            
+                <form className={contactPage.container} action="https://formspree.io/jfernando0911@gmail.com" method="POST">
+                    <div className={contactPage.name}>
                         <p>Name</p>
-                        <input type="text" placeholder="Please enter your name"/>
+                        <input type="text" placeholder="Please enter your name" name="name"/>
                     </div>
-                    <div>
+                    <div className={contactPage.email}>
                         <p>Email</p>
-                        <input type="email" placeholder="Please enter your email"/>
+                        <input type="email" placeholder="Please enter your email" name="_replyto"/>
                     </div>
-                    <div>
+                    <div className={contactPage.message}>
                         <p>Message</p>
-                        <textarea>
+                        <textarea name="message">
 
                         </textarea>
                     </div>
-                </div>  
+                    <div>
+                        <input type="submit" value="Send"/>
+                    </div>
+
+                </form>  
 
             </Container>
         </div>
